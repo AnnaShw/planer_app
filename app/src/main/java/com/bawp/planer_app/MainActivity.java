9904c80433918f4fe7e47b1b8e96089a3015f96a
Anna Rabynovych <rabynan@ac.sce.ac.il>
2021-02-11 18:19:34 +0200

    AddTask's fragment was made
    AddTask's fragment was made with option add task's and came back to previous activity

--- app/src/main/java/com/bawp/planer_app/MainActivity.java
+++ app/src/main/java/com/bawp/planer_app/MainActivity.java
@@ -1,11 +1,18 @@
 package com.bawp.planer_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -21,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         String currentDateandTime = sdf.format(new Date());
         textView.setText(currentDateandTime);
+
+        //Here we open fragment by clicking on Add task button
+        Button addTask=(Button)findViewById(R.id.addTaskButton);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddTaskFragment AddTaskfragment=new AddTaskFragment();
+                FragmentManager manager=getSupportFragmentManager();
+                manager.beginTransaction().add(R.id.main,AddTaskfragment).commit();
+
+            }
+        });
+        Bundle bundle=getIntent().getExtras();
+        if( bundle!=null){
+            if(bundle.getString("task")!=null){
+                Toast.makeText(getApplicationContext(),"Let's add your"+bundle.getString("task"),Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
