838da65cdb89bfd1afe1447c77352b957059a64c
Anna Rabynovych <rabynan@ac.sce.ac.il>
2021-02-11 15:30:45 +0200

    Splash_Screen+design work on main activity
    Several sings were made:
    1)Splash screen and moving into main activity
    2)Display current date for more convenient user experience
    3)Add display "Add task for today".
    4)design work on how application looks

--- app/src/main/java/com/bawp/planer_app/MainActivity.java
+++ app/src/main/java/com/bawp/planer_app/MainActivity.java
@@ -2,13 +2,24 @@ package com.bawp.planer_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView=findViewById(R.id.displayDate);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String currentDateandTime = sdf.format(new Date());
+        textView.setText(currentDateandTime);
     }
 }
