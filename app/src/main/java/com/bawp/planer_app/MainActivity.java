5e2596fd3dc4f8d503a2762d48d5b63356b8e0a3
Anna Rabynovych <rabynan@ac.sce.ac.il>
2021-02-12 21:58:48 +0200

    Changes:
    1)Deleted fragment for adding task
    2)adding task in the main activity+immediately refreshing view list

--- app/src/main/java/com/bawp/planer_app/MainActivity.java
+++ app/src/main/java/com/bawp/planer_app/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.LinkedList;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
@@ -16,35 +24,47 @@ import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import android.widget.ListView;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    //variables
+    ImageButton save;
+    EditText name;
+    ArrayAdapter arrayAdapter;
+    LinkedList<String>lst;
+    private ListView Lview;
+
+
     @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView textView=findViewById(R.id.displayDate);
+        lst = new LinkedList<String>();
+        //Display current date
+        TextView textView = findViewById(R.id.displayDate);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         String currentDateandTime = sdf.format(new Date());
         textView.setText(currentDateandTime);
 
-        //Here we open fragment by clicking on Add task button
-        Button addTask=(Button)findViewById(R.id.addTaskButton);
-        addTask.setOnClickListener(new View.OnClickListener() {
+        name = findViewById(R.id.taskInput);
+        Lview = findViewById(R.id.listView);
+        save=findViewById(R.id.addTask);
+        save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AddTaskFragment AddTaskfragment=new AddTaskFragment();
-                FragmentManager manager=getSupportFragmentManager();
-                manager.beginTransaction().add(R.id.main,AddTaskfragment).commit();
-
+                lst.add(name.getText().toString());
+                arrayAdapter=new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, lst);
+                Lview.setAdapter(arrayAdapter);
+                arrayAdapter.notifyDataSetChanged();
+                Lview.invalidateViews();
+                Lview.refreshDrawableState();
             }
         });
-        Bundle bundle=getIntent().getExtras();
-        if( bundle!=null){
-            if(bundle.getString("task")!=null){
-                Toast.makeText(getApplicationContext(),"Let's add your"+bundle.getString("task"),Toast.LENGTH_SHORT).show();
-            }
-        }
+
+        //Here we open fragment by clicking on Add task button
+
+
     }
 }
---
+++ app/src/test/java/com/bawp/planer_app/AddTaskFragmentTest.java
@@ -0,0 +1,12 @@
+package com.bawp.planer_app;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class AddTaskFragmentTest extends TestCase {
+    @Test
+    public void testAddToListTasks() {
+
+    }
+}
